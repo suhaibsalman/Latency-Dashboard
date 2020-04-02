90322bed53232f1d0edf710c72a2f1cf00316a4e
suhaib salman <suhypsalman@gmail.com>
2020-04-02 20:28:32 +0300

    version 0.0.6

--- src/main/java/com/example/demo/MbLatencyTool1Application.java
+++ src/main/java/com/example/demo/MbLatencyTool1Application.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringReader;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -13,8 +15,16 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.regex.Pattern;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.apache.log4j.Logger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import Utils.Constants;
 import dao.JDBCDaoImpl;
@@ -33,14 +43,9 @@ public class MbLatencyTool1Application {
 
 	private static final String ID_TAG = "sId:";
 	private static final String END_SEPARATOR = "]";
-	private static final String END_TAG_SEPARATOR = "<";
-	private static final String FIRST_ROW_INDECATOR = "DEBUG";
-	private static final String SERVICE_NAME_TAG = "<ns2:";
-	private static final String LOGIN_REQUEST_TAG = "<ns2:LoginRequest>";
-	private static final String LOGIN_RRESPONSE_TAG = "<ns2:LoginResponse>";
-	private static final String CHANNEL_TRX_REF_TAG = "ChannelTrxRef>";
-	private static final String REQUEST_TAG = "<requestId>";
-	private static final String END_ENVELOPE_TAG = "</soapenv:Envelope>";
+	
+	private static final String START_ENVELOP = "<soapenv:Envelope";
+	private static final String END_ENVELOP = "</soapenv:Envelope>";
 
 	private static final String MIDDLE_WARE = "MW";
 	private static final String INTERNET_BANKING = "Internet banking";
@@ -51,7 +56,10 @@ public class MbLatencyTool1Application {
 
 	public static void main(String[] args) {
 		logger.info("Start Java Application");
+		
 		repeatEachXSecond(getRepeatedTime());
+        
+		//readFormXMLFile(getFilePath());
 	}
 
 	private static long getRepeatedTime() {
@@ -63,27 +71,33 @@ public class MbLatencyTool1Application {
 		TimerTask myTask = new TimerTask() {
 			@Override
 			public void run() {
-				readFormFile(getFilePath());
+				readFormXMLFile(getFilePath());
 			}
 		};
 
 		timer.schedule(myTask, 0, rTime);
 	}
-
-	private static void readFormFile(String path) {
+	
+	private static void readFormXMLFile(String path) {
 		BufferedReader br = null;
+		String result = "";
 		try {
 			br = new BufferedReader(new FileReader(path));
 
+			if(getStartPos() == getFileLength()) {
+				return;
+			}
 			br.skip(getStartPos());
-
+	
+			modifyLastPos(getFileLength());
+			
 			String contentLine = br.readLine();
 			while (contentLine != null) {
-				incLastPos(contentLine.length());
-				validate(contentLine.trim());
+				result = result + contentLine;
 				contentLine = br.readLine();
 			}
-
+		
+			SplitRequest(result);
 		} catch (FileNotFoundException e) {
 			logger.error("File not found");
 		} catch (IOException ioe) {
@@ -98,32 +112,90 @@ public class MbLatencyTool1Application {
 			}
 		}
 	}
+	
+	private static void convertStringToXmlDoc(String request) {
 
-	private static void incLastPos(long charNum) {
-		lastRowPos = lastRowPos + charNum;
+		try {
+			String[] parts = request.split(Pattern.quote(START_ENVELOP));
+			
+			String xml = START_ENVELOP + parts[1];
+			
+			Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+					.parse(new InputSource(new StringReader(xml)));
+		
+			date = getDate( parts[0]);
+			sessionID = getSessionID(parts[0]);
+			serviceName = getServiceName(xml);
+			channelTrxRef = getChannelTrxRef(doc);
+			requestId = getRequestID(doc);
+			
+			if (!requestId.isEmpty()) {
+				type = INTERNET_BANKING;
+			} else if (!channelTrxRef.isEmpty()) {
+				type = MIDDLE_WARE;
+			} else {
+				type = "";
+			}
+			
+			insertRecord(getRecord());
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	private static String getServiceName(String xml) {
+		int endPos = xml.indexOf(" ", xml.indexOf("<ns2:"));
+		String sName = xml.substring(xml.indexOf("<ns2:")+5,endPos);
+		
+		return sName;
+	}
+
+	private static String getChannelTrxRef(Document doc) {
+		NodeList nodeList = doc.getElementsByTagName("ns3:Channel");
+		
+		if (nodeList.getLength() > 0) {
+			type = MIDDLE_WARE;
+			Element node = (Element) nodeList.item(0);
+			return node.getElementsByTagName("ns3:ChannelTrxRef").item(0).getTextContent();
+		} else {
+			return "";
+		}
+	}
+	
+	private static String getRequestID(Document doc) {
+		NodeList nodeList = doc.getElementsByTagName("RequestHeader");
+		
+		if (nodeList.getLength() > 0) {
+			Element node = (Element) nodeList.item(0);
+			return node.getElementsByTagName("requestId").item(0).getTextContent();
+		} else {
+			return "";
+		}
+	}
+
+	private static void SplitRequest(String fContent) {
+		String[] parts = fContent.split(Pattern.quote(END_ENVELOP));
+		
+		for(int i=0; i<parts.length; i++) {
+			if(!parts[i].trim().isEmpty()) {
+				convertStringToXmlDoc(parts[i]+ END_ENVELOP);
+			}
+		
+		}
+		
+	}
+	
+	private static void modifyLastPos(long charNum) {
+		lastRowPos = charNum;
 	}
 
 	private static long getStartPos() {
 		return lastRowPos;
-	}
-
-	private static void validate(String line) {
-		if (line.contains(FIRST_ROW_INDECATOR)) {
-			date = getDate(line);
-			sessionID = getSessionID(line);
-		} else if (line.contains(LOGIN_REQUEST_TAG) || line.contains(LOGIN_RRESPONSE_TAG)) {
-			return;
-		} else if (line.contains(SERVICE_NAME_TAG)) {
-			serviceName = getServiceName(line);
-		} else if (line.contains(CHANNEL_TRX_REF_TAG)) {
-			type = MIDDLE_WARE;
-			channelTrxRef = getChannelTrxRef(line);
-		} else if (line.contains(REQUEST_TAG)) {
-			type = INTERNET_BANKING;
-			requestId = getRequestID(line);
-		} else if (line.contains(END_ENVELOPE_TAG)) {
-		    insertRecord(getRecord());
-		}
 	}
 
 	private static void insertRecord(LatencyDashboard latencyDashboard) {
@@ -136,23 +208,6 @@ public class MbLatencyTool1Application {
 		resetParameter();
 	}
 
-	private static String getRequestID(String line) {
-		int startPos = line.indexOf(REQUEST_TAG) + REQUEST_TAG.length();
-		int endPos = line.indexOf(END_TAG_SEPARATOR, startPos);
-
-		return line.substring(startPos, endPos);
-	}
-
-	private static String getChannelTrxRef(String line) {
-		int startPos = line.indexOf(CHANNEL_TRX_REF_TAG) + CHANNEL_TRX_REF_TAG.length();
-		int endPos = line.indexOf(END_TAG_SEPARATOR, startPos);
-
-		return line.substring(startPos, endPos);
-	}
-
-	private static String getServiceName(String line) {
-		return line.substring(SERVICE_NAME_TAG.length(), line.length());
-	}
 
 	private static String getSessionID(String line) {
 		int startPos = line.indexOf(ID_TAG) + ID_TAG.length();
@@ -241,4 +296,11 @@ public class MbLatencyTool1Application {
 		}
 		return date != null;
 	}
+	
+	private static long getFileLength() {
+        File f = new File(getFilePath()); 
+        
+        return f.length();
+	}
+	
 }
